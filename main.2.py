e5a708d2552ab5bf2cb316c90e632bbbab2f8dcc
AntonTevseev <91051811+AntonTevseev@users.noreply.github.com>
2021-11-24 20:50:34 +0200

    Create main.2.py

---
+++ main.2.py
@@ -0,0 +1,21 @@
+
+class Student(object):
+    def __init__(self,name,grade):
+        self.name=name
+        self.grade=grade
+grade ={
+    'exam_max':100,
+    'lab_max':5,
+    'lab':10,
+    'k':0.6
+}
+def times_exam(self, tim, lev):
+    self.times=tim
+    self.level=lev
+def times_lab(self, time:int, lab:int):
+    self.time=time
+    self.lab=lab
+
+times_exam("exam", "2", "100")
+times_lab("lab", "2", "5")
+s=Student("Sasha", grade)
